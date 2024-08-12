a25669197b61d71120fdaee8533c6f98539956d3
01Ryuk <imoukhuedeemmanuel5@gmail.com>
2024-08-12 15:49:53 +0100

    displaying information in the view page
    displayed the todo title and description in the view page

--- src/pages/View.js
+++ src/pages/View.js
@@ -1,43 +1,41 @@
-import React, { useEffect, useState } from 'react'
-import Header from '../common/Header'
-import Footer from '../common/Footer'
-import axios from 'axios'
-import { useLocation } from 'react-router'
+import React, { useEffect, useState } from "react";
+import Header from "../common/Header";
+import Footer from "../common/Footer";
+import axios from "axios";
+import { useLocation } from "react-router";
 
 const View = () => {
-const location=useLocation()
-  const [title, setTitle] = useState()
-  const [description, setDescription] = useState()
-  const todoid=location.pathname.split('/')[2]
-    useEffect(() => {
+  const location = useLocation();
+  const [title, setTitle] = useState();
+  const [description, setDescription] = useState();
+  const todoid = location.pathname.split("/")[2];
+  useEffect(() => {
     axios
-        .post("http://localhost:8000/api/view/", {todoid})
-        .then((response) => {
-          if (response.data.status === 200) {
-            setTitle(response.data.data.title);
-            setDescription(response.data.data.description);
-          } else {
-            alert("Failed to create todo!");
-          }
-        })
-        .catch((error) => {
-          console.error("Error:", error);
-        })
-  }, []);
-  
+      .post("http://localhost:8000/api/view/", { todoid })
+      .then((response) => {
+        if (response.data.status === 200) {
+          setTitle(response.data.data.title);
+          setDescription(response.data.data.description);
+        } else {
+          alert("Failed to create todo!");
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }, [todoid]);
 
   return (
     <>
-    <Header />
-    <div className='todocontent'>
-      <h1>Todo Title</h1>
-      <h6>Todo Description</h6>
-  </div>
- 
-    
-    <Footer />
-    </>
-  )
-}
+      <Header />
+      <div className="todocontent">
+        <h1>{title}</h1>
+        <h6>{description}</h6>
+      </div>
 
-export default View
+      <Footer />
+    </>
+  );
+};
+
+export default View;
